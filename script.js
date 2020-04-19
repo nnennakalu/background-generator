1c1b586f938969e142f5938f8c200ac7d5b911aa
sophiekalu <sophiekalu.developer@gmail.com>
2020-04-19 09:02:09 +0100

    Styled Current  CSS background code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var css = document.querySelector("h3");
+var css = document.querySelector("code");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
