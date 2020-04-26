4e942c08f0f967ac18fcb3d875c89055d41d17ff
sophiekalu <sophiekalu.developer@gmail.com>
2020-04-26 11:17:23 +0100

    choose orientation

--- script.js
+++ script.js
@@ -2,18 +2,39 @@ var css = document.querySelector("code");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var orientation = document.querySelectorAll('input[type="radio"]');
+console.log(orientation);
 
-function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
 
-	css.textContent = body.style.background + ";";
+function setGradient(e) {
+	currentTarget = e.target;
+	console.log(currentTarget);
+	console.log(currentTarget.value);
+
+	if (currentTarget.type === "radio") {
+
+		body.style.background =
+			"linear-gradient(" +
+			currentTarget.value +
+			", " +
+			color1.value +
+			"," +
+			color2.value +
+			")";
+	}
+
+
+	css.textContent = body.style.background;
 }
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+for (let i = 0; i < orientation.length; i++) {
+	const element = orientation[i];
+	element.addEventListener('click', function (e) {
+		setGradient(e);
+	}, false);
+
+}
