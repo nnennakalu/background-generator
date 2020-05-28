0758ab92b392d3f9b0efd6e7ed619798dcc341f9
sophiekalu <sophiekalu.developer@gmail.com>
2020-05-28 12:57:58 +0100

    update committed

--- script.js
+++ script.js
@@ -3,35 +3,52 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 var orientation = document.querySelectorAll('input[type="radio"]');
-console.log(orientation);
 
+
+// Function Declarations
 function currentSettings() {
-	var CSSprop = window.getComputedStyle(body, null).getPropertyValue("background-image");;
+	var CSSprop = window.getComputedStyle(body, null).getPropertyValue("background-image");
 	css.textContent = CSSprop;
 }
-
 currentSettings();
+
+function getRandomValues() {
+	return '#' + (function co(lor) {
+		return (lor += [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random() * 16)]) &&
+			(lor.length == 6) ? lor : co(lor);
+	})('');
+}
+
+function cssStyle(currentTarget) {
+	body.style.background =
+		"linear-gradient(" +
+		currentTarget.value +
+		", " +
+		color1.value +
+		"," +
+		color2.value +
+		")";
+
+}
 
 function setGradient(e) {
 	currentTarget = e.target;
-	console.log(currentTarget);
-	console.log(currentTarget.value);
 
 	if (currentTarget.type === "radio") {
+		cssStyle(currentTarget);
+	}
 
-		body.style.background =
-			"linear-gradient(" +
-			currentTarget.value +
-			", " +
-			color1.value +
-			"," +
-			color2.value +
-			")";
+	if (currentTarget.value === "circle") {
+		color1.value = getRandomValues();
+		color2.value = getRandomValues();
+		cssStyle(currentTarget);
 	}
 
 	currentSettings();
 }
 
+
+// Event Listeners
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
